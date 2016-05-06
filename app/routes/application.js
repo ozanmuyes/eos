1d64d2c950fea7fde13a275c51d862fef5f71173
ozanmuyes <ozanmuyes@gmail.com>
2016-05-06 08:22:15 +0300

    Document Title package added and route titles set

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
+  title: function (tokens) {
+    return "Eos - " + tokens.join(" - ");
+  },
+
   actions: {
     error(error, transition) {
       if (error) {
--- app/routes/users/create.js
+++ app/routes/users/create.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  titleToken: "Yeni Kullanıcı",
+
   model () {
     return this.store.createRecord("user");
   }
